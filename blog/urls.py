c09cc0bd18ca3883416e712eb0730624d8ba7783
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-15 19:49:05 +0300

    Added blog detail view

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles', include('articles.urls'))
+    path('articles/', include('articles.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
