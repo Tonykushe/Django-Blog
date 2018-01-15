1a6701a0a7276c795ce74e903a15af92c3a853f1
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-15 23:34:20 +0300

    Added user accounts app

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls'))
+    path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
