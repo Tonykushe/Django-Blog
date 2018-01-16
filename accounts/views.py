5d6de09712f69e698c4a7fc5c77f6bb6a577e851
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-16 11:39:16 +0300

    Added login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup(request):
@@ -9,8 +9,17 @@ def signup(request):
                         form.save()
                         # Log user in
                         return redirect('articles:list')
-                else:
-                        return render(request, 'accounts/signup.html', { 'form': form })
         else:
                 form = UserCreationForm()
-                return render(request, 'accounts/signup.html', { 'form' : form })
+        return render(request, 'accounts/signup.html', { 'form' : form })
+
+def login(request):
+        if request.method == 'POST':
+                form = AuthenticationForm(data = request.POST)
+                if form.is_valid():
+                        # Log user in
+                        return redirect('articles:list')
+        else:
+                form = AuthenticationForm()
+        return render(request, 'accounts/login.html', { 'form' : form })
+
