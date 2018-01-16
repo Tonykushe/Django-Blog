6be019e182c6a2094c0a82536343d8741d658eb7
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-16 11:56:26 +0300

    Users are now logged in

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
-def signup(request):
+def signup_view(request):
         if request.method == 'POST':
                 form = UserCreationForm(request.POST)
                 if form.is_valid():
-                        form.save()
+                        user = form.save()
                         # Log user in
+                        login(request, user)
                         return redirect('articles:list')
         else:
                 form = UserCreationForm()
         return render(request, 'accounts/signup.html', { 'form' : form })
 
-def login(request):
+def login_view(request):
         if request.method == 'POST':
                 form = AuthenticationForm(data = request.POST)
                 if form.is_valid():
                         # Log user in
+                        user = form.get_user()
+                        login(request, user)
                         return redirect('articles:list')
         else:
                 form = AuthenticationForm()
